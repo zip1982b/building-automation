83c86eed6baf6d2d7811f5a4f3c78ce00dbb0979
zip1982b <zip1982b@gmail.com>
2019-11-01 07:52:26 +0200

    test sqlalchemy

--- test_sa.py
+++ test_sa.py
@@ -1,8 +1,11 @@
 from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, ForeignKey
 
+from ba.settings import config, BASE_DIR
+
+DSN = "postgresql://{user}:{password}@{host}:{port}/{database}"
 metadata = MetaData()
 
-users = Table('users', metadata,
+users_1 = Table('users_1', metadata,
               Column('id', Integer, primary_key=True),
               Column('name', String),
               Column('fullname', String),
@@ -10,18 +13,20 @@ users = Table('users', metadata,
 
 addresses = Table('addresses', metadata,
                   Column('id', Integer, primary_key=True),
-                  Column('user_id', None, ForeignKey('users.id')),
+                  Column('user_id', None, ForeignKey('users_1.id')),
                   Column('email_address', String, nullable=False)
                   )
 
 
-engine = create_engine('sqlite:///:memory:', echo=True)
+db_url = DSN.format(**config['postgres'])
+
+engine = create_engine(db_url, echo=True)
 metadata.create_all(engine)
 
-ins = users.insert().values(name='zhan', fullname='Zhan Beshanov')
+ins = users_1.insert().values(name='zhan', fullname='Zhan Beshanov')
 conn = engine.connect()
 result = conn.execute(ins)
-
+print(result)
 
 
 
